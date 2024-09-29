2402e49fc6cd1565839fe28407186b0ec1ca11b8
RiceL123 <ericliao57@gmail.com>
2024-09-30 03:13:33 +1000

    :O

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
 
-import tailwind from "@astrojs/tailwind";
+import sitemap from '@astrojs/sitemap';
+
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+    site: 'https://example.com',
+    integrations: [mdx(), sitemap(), tailwind()],
 });
