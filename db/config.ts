0568b6c0f682a928529190da22f336ce3cb1e9ec
RiceL123 <ericliao57@gmail.com>
2025-01-14 01:07:27 +1000

    🚤

---
+++ db/config.ts
@@ -0,0 +1,22 @@
+import { defineDb, defineTable, column } from 'astro:db';
+
+const MetaData = defineTable({
+  columns: {
+    blog: column.text({primaryKey: true}),
+    views: column.number(),
+    likes: column.number()
+  }
+})
+
+const Comment = defineTable({
+  columns: {
+    author: column.text(),
+    body: column.text(),
+    date: column.date(),
+    blog: column.text({references: () => MetaData.columns.blog })
+  }
+})
+
+export default defineDb({
+  tables: { Comment, MetaData }
+});
