a77014c0b640fa374072fe4fe821720a02433c52
RiceL123 <ericliao57@gmail.com>
2025-01-17 01:01:02 +1000

    🤾‍♂️

--- db/config.ts
+++ db/config.ts
@@ -1,22 +1,22 @@
-import { defineDb, defineTable, column } from 'astro:db';
+import { defineDb, defineTable, column } from "astro:db";
 
 const MetaData = defineTable({
   columns: {
-    blog: column.text({primaryKey: true}),
+    blog: column.text({ primaryKey: true }),
     views: column.number(),
-    likes: column.number()
-  }
-})
+    likes: column.number(),
+  },
+});
 
 const Comment = defineTable({
   columns: {
     author: column.text(),
     body: column.text(),
     date: column.date(),
-    blog: column.text({references: () => MetaData.columns.blog })
-  }
-})
+    blog: column.text({ references: () => MetaData.columns.blog }),
+  },
+});
 
 export default defineDb({
-  tables: { Comment, MetaData }
+  tables: { Comment, MetaData },
 });
