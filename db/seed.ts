c9723fdcc94f269b42906d465b6fa9182932880b
Eric L <115810538+RiceL123@users.noreply.github.com>
2025-01-17 01:10:34 +1000

    server side db

---
+++ db/seed.ts
@@ -0,0 +1,17 @@
+import { db, MetaData, Comment } from 'astro:db';
+
+// https://astro.build/db/seed
+export default async function seed() {
+	await db.insert(MetaData).values([
+		{
+			blog: "advent-of-code-2024",
+			views: 21,
+			likes: 2
+		},
+		{
+			blog: "meta-analysis-on-reducing-screentime",
+			views: 40,
+			likes: 4
+		}
+	])
+}
