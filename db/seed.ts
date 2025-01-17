0945588c2fdbce27554fddde197a0a33aeccb8a0
Eric L <115810538+RiceL123@users.noreply.github.com>
2025-01-18 02:12:15 +1000

    Migrate to Astro5 (#4)

--- db/seed.ts
+++ db/seed.ts
@@ -1,17 +1,22 @@
-import { db, MetaData, Comment } from 'astro:db';
+import { db, MetaData, Comment } from "astro:db";
 
 // https://astro.build/db/seed
 export default async function seed() {
-	await db.insert(MetaData).values([
-		{
-			blog: "advent-of-code-2024",
-			views: 21,
-			likes: 2
-		},
-		{
-			blog: "meta-analysis-on-reducing-screentime",
-			views: 40,
-			likes: 4
-		}
-	])
+  await db.insert(MetaData).values([
+    {
+      blog: "advent-of-code-2024",
+      views: 21,
+      likes: 2,
+    },
+    {
+      blog: "meta-analysis-on-reducing-screentime",
+      views: 40,
+      likes: 4,
+    },
+    {
+      blog: "squarea_10",
+      views: 1,
+      likes: 0,
+    },
+  ]);
 }
