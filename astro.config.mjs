dc5bda839569eb9a818a5b4bc90520115d474407
RiceL123 <ericliao57@gmail.com>
2025-01-21 14:02:11 +1000

    likes

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,8 +8,11 @@ import tailwind from '@astrojs/tailwind';
 
 import db from '@astrojs/db';
 
-// https://astro.build/config
+import vercel from '@astrojs/vercel';
+
 export default defineConfig({
-    site: 'https://ricel123.vercel.app',
-    integrations: [mdx(), sitemap(), tailwind(), db()],
+  site: 'https://ricel123.vercel.app',
+  integrations: [mdx(), sitemap(), tailwind(), db()],
+  output: 'static',
+  adapter: vercel(),
 });
--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,19 +1,19 @@
-// import { db, sql, MetaData, eq } from "astro:db";
-// import { defineAction } from "astro:actions";
-// import { z } from "astro:schema";
+import { db, sql, MetaData, eq } from "astro:db";
+import { defineAction } from "astro:actions";
+import { z } from "astro:schema";
 
-// export const server = {
-//   incrementLikes: defineAction({
-//     input: z.object({
-//       blog: z.string(),
-//     }),
-//     handler: async (input) => {
-//       const updatedLikes = await db
-//         .update(MetaData)
-//         .set({ likes: sql`${MetaData.likes} + 1` })
-//         .where(eq(MetaData.blog, input.blog))
-//         .returning();
-//       return updatedLikes;
-//     },
-//   }),
-// };
+export const server = {
+  incrementLikes: defineAction({
+    input: z.object({
+      blog: z.string(),
+    }),
+    handler: async (input) => {
+      const updatedLikes = await db
+        .update(MetaData)
+        .set({ likes: sql`${MetaData.likes} + 1` })
+        .where(eq(MetaData.blog, input.blog))
+        .returning();
+      return updatedLikes;
+    },
+  }),
+};
