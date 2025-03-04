19c9f791d4fe42470023df39b8efd50ee951a0a3
Eric L <email@email.com>
2025-03-04 22:58:51 +1100

    0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
-import db from '@astrojs/db';
+import db from "@astrojs/db";
 
-import vercel from '@astrojs/vercel';
+import vercel from "@astrojs/vercel";
 
 export default defineConfig({
-  site: 'https://ricel123.vercel.app',
+  site: "https://ricel123.vercel.app",
   integrations: [mdx(), sitemap(), tailwind(), db()],
-  output: 'static',
+  output: "static",
   adapter: vercel(),
 });
