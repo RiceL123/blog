78a0461ccc599e52c6e13bc43759e30ec95ccb95
Eric L <115810538+RiceL123@users.noreply.github.com>
2025-06-11 18:38:36 +1000

    Listening to silence (#8)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,18 @@
-// @ts-check
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-
-import sitemap from "@astrojs/sitemap";
-
-import tailwind from "@astrojs/tailwind";
-
-import db from "@astrojs/db";
-
-import vercel from "@astrojs/vercel";
-
+import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import db from '@astrojs/db';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel';
+// https://astro.build/config
 export default defineConfig({
   site: "https://ricel123.vercel.app",
-  integrations: [mdx(), sitemap(), tailwind(), db()],
-  output: "static",
-  adapter: vercel(),
+
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  output: 'static',
+  integrations: [db(), mdx(), sitemap()],
+  adapter: vercel()
 });
